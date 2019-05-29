c4d2c3ddc1928b2f71373e725bb0abf656f3cc58
Lorenzo Pattori <lpattori@gmail.com>
2019-05-29 17:29:00 -0300

    Vinchucas

--- app/server.py
+++ app/server.py
@@ -32,7 +32,11 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        data2 = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(),
+                                                   size=224).normalize(imagenet_stats)
+        learn = create_cnn(data2, models.resnet34)
+        learn.load(path / export_file_name)
+#       learn = load_learner(path, export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
